43260fb267ff064bc11d56bf4ec1fc34b204f0ce
dhanushMindia <dhanush.mendu@gmail.com>
2025-06-28 02:54:19 +0530

    Update jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || "your-super-secret-jwt-key-here",,
+      secretOrKey: process.env.JWT_SECRET || "your-super-secret-jwt-key-here",
     });
   }
 
