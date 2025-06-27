7e3adf3f1a97617bbb246ba2b378581bc8c565d8
dhanushMindia <dhanush.mendu@gmail.com>
2025-06-28 03:00:24 +0530

    revert to working Google strategy

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private authService: AuthService) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      // Correctly loads credentials from environment variables
+       clientID: '301245041185-eihdb26ldmnk3ahbj77s1i98dvicepao.apps.googleusercontent.com', // Replace with your actual Google Client IDAdd commentMore actions
+      clientSecret: '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', // Replace with your actual Google Client Secret
       callbackURL: 'https://ec2-43-205-113-7.ap-south-1.compute.amazonaws.com/auth/google/callback',
       scope: ['email', 'profile'],
     });
@@ -19,7 +19,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     accessToken: string,
     refreshToken: string,
     profile: any,
-    done: VerifyCallback
+    done: VerifyCallback,
   ): Promise<any> {
     console.log('Google profile:', JSON.stringify(profile, null, 2));
     const { name, emails, photos } = profile;
