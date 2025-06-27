aced3c4a22fe9484f773c76f407d6cdba0d74b7f
dhanushMindia <dhanush.mendu@gmail.com>
2025-06-28 02:33:06 +0530

    Update main.ts to app.listen to 3000 port on 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
   
-  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
